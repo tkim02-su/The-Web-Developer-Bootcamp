8d59aaa6f8eb91a88819edb21298c2b3bf2f2e35
tkim02-su <tkim02@syr.edu>
2021-05-02 21:45:53 -0400

    Create JavaScript-Practice.js

---
+++ JavaScript-Practice.js
@@ -0,0 +1,45 @@
+function isEven(num) {
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+    if (num % 2 === 0) {
+        console.log("even")
+    }
+
+    //AND THIS LINE ↑↑↑↑↑
+}
+function getColor(phrase) {
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+    if (phrase === 'stop') {
+        console.log('red')
+    } else if (phrase === 'slow') {
+        console.log('yellow')
+    } else if (phrase === 'go') {
+        console.log('green')
+    } else {
+        console.log('purple')
+    }
+
+    //AND THIS LINE ↑↑↑↑↑
+}
+// Change the value of num, so that "YOU GOT ME!" prints out
+const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :) 
+
+// DO NOT TOUCH ANYTHING BELOW (please) 
+if (num <= 100) {
+    if (num >= 50) {
+        console.log("HEY!");
+    }
+} else {
+    if (num < 103) {
+        if (num % 2 === 0) {
+            console.log("YOU GOT ME!");
+        }
+    }
+}
+const mystery = 'Paaaa7'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+
+
+// LEAVE THIS CODE ALONE! (pretty please)
+if (mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1) {
+    console.log("YOU GOT IT!!!");
+}
