60ec4df803d95d382709a3acfea959373591228b
tkim02-su <tkim02@syr.edu>
2021-05-03 21:29:33 -0400

    Guessing Game

--- JavaScript-Practice.js
+++ JavaScript-Practice.js
@@ -43,3 +43,64 @@ const mystery = 'Paaaa7'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
 if (mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1) {
     console.log("YOU GOT IT!!!");
 }
+
+
+const planets = ['The Moon', 'Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+planets.shift('The Moon');
+planets.push('Saturn');
+planets.unshift('Mercury')
+
+
+// Nested Arrays Exercise
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
+
+airplaneSeats[3][1] = 'Hugo';
+
+
+// Object Access Exercise
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+let fullAddress = restaurant['address'] + restaurant['city'] + restaurant['state'] + restaurant['zipcode'];
+
+// for loops
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let i = 0; i <= people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
+
+// Nested Loops
+
+const seatingChart = [
+    ['kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`ROW #${i + 1}`)
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j])
+    }
+}
