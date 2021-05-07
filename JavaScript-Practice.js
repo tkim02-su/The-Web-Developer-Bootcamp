e7592bded5c2959c4824b8da18450117599f7303
tkim02-su <tkim02@syr.edu>
2021-05-07 17:32:33 -0400

    Update JavaScript-Practice.js

--- JavaScript-Practice.js
+++ JavaScript-Practice.js
@@ -186,7 +186,28 @@ function allEvens(str) {
     return str.every(num => num % 2 === 0);
 }
 
+// getElementById Practice
 
+// HTML:
+// <!DOCTYPE html>
+// <html>
+
+// <head>
+//     <title>Unicorn</title>
+// </head>
+
+// <body>
+//     <!--DON'T TOUCH THIS FILE PLEASE!-->
+//     <h1 id="mainheading">I &hearts; Unicorns</h1>
+//     <img id="unicorn" src="https://www.flaticon.com/svg/static/icons/svg/3468/3468081.svg" alt="" width="200px">
+//     <!-- Icon made by https://www.flaticon.com/authors/freepik  -->
+
+// </body>
+
+// </html>
+
+const image = document.getElementById("unicorn");
+const heading = document.getElementById("mainheading");
 
 
 
